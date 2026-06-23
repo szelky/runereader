c3bb1c3a23e467d52af0f71c28e93ce7deb518d8
szelky <yigitkose7@gmail.com>
2026-06-23 17:03:14 +0300

    testing flet

--- src/main.py
+++ src/main.py
@@ -1,11 +1,22 @@
 import flet as ft
+from scraper import *
 
 def main(page: ft.Page):
     page.title = "Rune Novel Reader"
     # page.vertical_alignment = ft.TextAlign.CENTER
     page.horizontal_alignment = ft.TextAlign.CENTER
-    result = ft.Text(value="Rune Novel Reader", margin=40, size=20, color="#76e6a3")
 
-    page.add(result)
+    def home():
+        page.clean()
+        title = ft.Text(value="Rune Novel Reader", margin=25, size=20, color="#76e6a3")
+        page.add(title, ft.Text(value="test"), ft.Button("Read", on_click=read))
+
+    def read():
+        page.clean()
+        test = ft.Text('"I always knew Dior was an idiot. It was just a matter of time."\n\n' \
+        'Noel passed them without slowing, eyes forward, jaw tight.', size=16, margin=25)
+        page.add(test, ft.Button("Home", on_click=home))
+
+    home()
 
 ft.run(main)
---
+++ src/scraper.py
@@ -0,0 +1,5 @@
+import json
+import httpx
+from bs4 import BeautifulSoup
+import asyncio
+
