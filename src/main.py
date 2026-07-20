cfd975c4739c8cda586c9f8fb51434fffebaba1c
szelky <yigitkose7@gmail.com>
2026-07-20 16:11:58 +0300

    added multi page

--- src/main.py
+++ src/main.py
@@ -8,15 +8,16 @@ def main(page: ft.Page):
 
     def home():
         page.clean()
-        title = ft.Text(value="Rune Novel Reader", margin=25, size=20, color="#76e6a3")
-        page.add(title, ft.Text(value="test"), ft.Button("Read", on_click=read))
+        title = ft.Text(value="Rune Novel Reader", margin=25, size=24, color="#76e6a3")
+        page.add(title, ft.Text(value="Placeholder", size=16), ft.Image(src="splash_android.png"), ft.Button("Read", on_click=read))
 
     def read():
         page.clean()
-        test = ft.Text('"I always knew Dior was an idiot. It was just a matter of time."\n\n' \
-        'Noel passed them without slowing, eyes forward, jaw tight.', size=16, margin=25)
-        page.add(test, ft.Button("Home", on_click=home))
+        content = ft.Text('"I always knew Dior was an idiot. It was just a matter of time."\n\n' \
+        'Noel passed them without slowing, eyes forward, jaw tight.', size=16)
+        page.add(ft.Text(value="\n"), content, ft.Divider(), ft.Button("Home", on_click=home))
 
     home()
 
-ft.run(main)
+if __name__ == "__main__":
+    ft.run(main)
